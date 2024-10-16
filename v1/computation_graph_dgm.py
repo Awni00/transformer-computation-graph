1c949e79b2b7211b79fa65b417dcad756e30518e
Siyu <siyuc3141@gmail.com>
2024-10-15 23:28:18 -0400

    support math expression

--- v1/computation_graph_dgm.py
+++ v1/computation_graph_dgm.py
@@ -40,210 +40,6 @@ class Tokenizer():
     def encode_string(self, string, sep=' '):
         return self.encode_tokens(string.split(sep))
 
-class ComputationGraphDGM():
-    def __init__(self, var_vocab, mod_val, function_map):
-        """A class for generating examples of computation graphs and corresponding "language modeling" prompts.
-
-
-        Parameters
-        ----------
-        var_vocab : list[str]
-            list of variable names
-        mod_val : int
-            base for modular arithmetic in computation graph
-        function_map : dict[str, function]
-            dictionary mapping function names to functions that take a list of integers and return an integer
-        """
-
-        # NOTE: for now, functions are assumed to accept any number of arguments (except for leaf assignmnet)
-        # TODO: generalize to allow each type of function to accept a specific number of arguments
-
-        self.var_vocab = var_vocab
-
-        self.mod_val = mod_val
-        self.numeric_vocab = [str(v) for v in range(mod_val)]
-
-        self.function_map = function_map
-        self.func_vocab = list(function_map.keys())
-
-        self.operand_vocab = [',', '(', ')']
-
-        self.equal_token = '->'
-        self.query_token = '<query>'
-        self.answer_token = '<answer>'
-        self.eq_sep_token = '<eq_sep>'
-        self.sep_token = '<sep>'
-
-
-        self.vocab = self.var_vocab + self.numeric_vocab + self.func_vocab + self.operand_vocab
-        self.vocab += [self.equal_token, self.query_token, self.answer_token, self.eq_sep_token, self.sep_token]
-        self.tokenizer = Tokenizer(self.vocab)
-
-    def sample_example(self, n_vars, func_degree, query_var='random', verbose=False):
-        # randomly sample a computation graph example; return a dictionary with keys: prompt, edges, func_annotations, var_top_order, node_vals, query_prompt
-        sample = self._sample_computation_graph_example(n_vars, func_degree, verbose=verbose)
-
-        # solve computation and add `node_vals` key to `sample` dict`. `node_vals` is a dictionary of solved node values (i.e., map each var to its value)
-        self._solve_sample(sample, verbose=verbose)
-
-        # create query prompt and add `query_prompt` key to `sample` dict. `query_prompt` is a list of tokens
-        # query prompt is a copy of the prompt with a randomly sampled query about the graph and answer appended
-        self._create_query_prompt(sample, query_var=query_var, verbose=verbose)
-
-        return sample
-
-    def _sample_computation_graph_example(self, n_vars, func_degree, verbose=False):
-
-        # first, sample a topological order of variables for computation DAG
-        var_top_order = list(np.random.choice(self.var_vocab, size=n_vars, replace=False))
-
-        edges = [] # for storing edges in DAG
-        func_annotations = dict() # for storing function annotations for each variable
-        prompt = [] # for storing prompt
-
-        if verbose:
-            print(f'topological order of variables in DAG: {var_top_order}')
-
-        for idx, var in enumerate(var_top_order):
-            # check if leaf node
-            if idx < func_degree: # for now, first func_degree variables are leaf nodes (because too few possible children)
-                var_val = random.choice(self.numeric_vocab)
-                edges.append((var_val, var))
-                func_annotations[var] = "leafValueAssignment"
-                prompt += [var_val, self.equal_token, var, self.eq_sep_token]
-                if verbose:
-                    print(f'{var} <- {var_val}')
-            # if not leaf node, children are randomly chosen from preceeding variables (wrt topological order)
-            # node is a randomly-selected function of its children
-            else:
-                # randomly sample children from preceeding variables
-                children = np.random.choice(var_top_order[:idx], size=func_degree, replace=False).tolist()
-
-                # randomly sample function
-                func = np.random.choice(self.func_vocab)
-                func_annotations[var] = func
-
-                for child in children:
-                    edges.append((child, var))
-
-                prompt += [func, '('] + list(','.join(children)) + [')'] + [self.equal_token, var, self.eq_sep_token]
-
-                if verbose:
-                    print(f'{var} <- {func}({", ".join(children)})')
-
-        if verbose:
-            print()
-            print(f"prompt: {' '.join(prompt)}")
-
-        return dict(prompt=prompt, edges=edges, func_annotations=func_annotations, var_top_order=var_top_order)
-
-    def _solve_sample(self, sample, verbose=False):
-        """add `node_vals` key to `sample` dict`. `node_vals` is a dictionary of solved node values (i.e., map each var to its value)"""
-        edges = sample['edges']
-        func_annotations = sample['func_annotations']
-        var_top_order = sample['var_top_order']
-
-        func_map = self.function_map.copy()
-        func_map['leafValueAssignment'] = leafValueAssignment # add leafValueAssignment function to function_map
-
-
-        G = nx.from_edgelist(edges, create_using=nx.DiGraph)
-        nx.set_node_attributes(G, func_annotations, 'func')
-        node_vals = {node: int(node) if node in self.numeric_vocab else None for node in G.nodes()}
-        nx.set_node_attributes(G, node_vals, 'val')
-
-        if verbose:
-            print()
-            print('Solving computation graph...')
-            print('initializing node values:')
-            print(node_vals)
-            print()
-            print('now iterating in topological order and solving for the value of each node...')
-            print()
-
-        for var in var_top_order:
-            children = list(G.predecessors(var)) # NOTE: unintuitively (for me), predecessors are called children in computation graphs
-            par_vals = [node_vals[child] for child in children]
-            if verbose:
-                print(f'Children({var}) = {[f"{p}={pv}" for p, pv in zip(children, par_vals)]}')
-            func_name = func_annotations[var]
-            func = func_map[func_name]
-            node_vals[var] = func(par_vals)
-            if verbose:
-                print(f'{var} <- {func_name}({[node_vals[child] for child in children]}) = {node_vals[var]}')
-                print()
-
-        sample['node_vals'] = node_vals
-
-    def _create_query_prompt(self, sample, query_var='random', verbose=False):
-        query_prompt = sample['prompt'].copy()
-        node_vals = sample['node_vals']
-        var_top_order = sample['var_top_order']
-
-        if query_var == 'last':
-            query_var = var_top_order[-1] # either last var in computation graph or randomly sample from vars
-        elif query_var == 'random':
-            query_var = np.random.choice(var_top_order)
-        else:
-            raise ValueError("query_var must be 'last' or 'random'")
-
-        query_prompt += [self.query_token, query_var, self.answer_token, str(node_vals[query_var])]
-
-        if verbose:
-            print(' '.join(query_prompt))
-
-        sample['query_var'] = query_var
-        sample['query_prompt'] = query_prompt
-
-        sample['tokenized_query_prompt'] = self.tokenizer.encode_tokens(query_prompt)
-
-def leafValueAssignment(children):
-    if len(children) == 1:
-        return children[0]
-    else:
-        raise ValueError("leafValueAssignment function should have exactly one child")
-
-def draw_topological_order(sample, numeric_vocab, query_node=None, rad=-0.8, figsize=(8,4), orientation='horizontal'):
-    """draw DAG according to topological order"""
-
-    G = nx.from_edgelist(sample['edges'], create_using=nx.DiGraph)
-
-    node_options = dict(edgecolors='tab:gray', node_size=800, alpha=0.8)
-    edge_options = dict(edge_color='tab:gray', alpha=0.9, width=1.5,)
-
-    # red if ancestor, blue if query node, black otherwise
-    if query_node is not None:
-        # ancestors of query node
-        ancestors = nx.ancestors(G, query_node)
-        node_colors = ['tab:red' if node in ancestors else 'tab:green' if node == query_node else 'tab:blue' for node in G.nodes()]
-    else:
-        node_colors = 'tab:blue'
-
-    top_order = sample['var_top_order']
-    numeric_nodes = [node for node in G.nodes() if node in numeric_vocab]
-    top_order = numeric_nodes + top_order
-
-    if orientation == 'horizontal':
-        pos = {node:(top_order.index(node),0) for node in G.nodes()}
-    else:
-        pos = {node:(0,top_order.index(node)) for node in G.nodes()}
-
-    fig, ax = plt.subplots(figsize=figsize)
-
-    nx.draw_networkx_nodes(G, node_color=node_colors, pos=pos, **node_options)
-
-    # list doesn't work for some reason...
-    # connection_styles = [f'arc3,rad={rad * (-1)**top_order.index(source)}' for source, target in G.edges()]
-
-    for edge in G.edges():
-        source, target = edge
-        nx.draw_networkx_edges(G, pos=pos, edgelist=[edge], connectionstyle=f'arc3,rad={rad*(-1)**top_order.index(source)}',
-            **edge_options, node_size=node_options['node_size'])
-
-    nx.draw_networkx_labels(G, pos=pos, font_color='whitesmoke')
-    plt.box(False)
-    return fig, ax
-
 
 class ADD:
     def __init__(self, mod_val) -> None:
@@ -295,7 +91,10 @@ class DAGWeightedNode:
         - func: the function used to combine the parent node's value with the current value
         """
         self.fan_in.append((parent_node, func))
-        self.depth = max(self.depth, parent_node.depth + 1)
+        if isinstance(parent_node, DAGWeightedNode):
+            self.depth = max(self.depth, parent_node.depth + 1)
+        else:
+            self.depth = 0
 
     def compute_value(self):
         """
@@ -326,6 +125,60 @@ class DAGWeightedNode:
             expression = f"{self.depth:<5}{self.name:<5} = {self.weight:<5} / {value: < 5} <-" + expression
             print(expression)
 
+    def to_math_expression(self):
+        """
+        Convert the node's information into a mathematical expression.
+        
+        Returns:
+        - A string representing the mathematical expression of the node.
+        """
+        if not self.fan_in:
+            return f"{self.name} = {self.weight}"
+        else:
+            expression = f"{self.name} = "
+            for i, (parent_node, func) in enumerate(self.fan_in):
+                operation = func.__name__ if hasattr(func, '__name__') else 'func'
+                if i > 0:
+                    expression += f" {operation} "
+                expression += f"{parent_node.name}"
+            return expression
+
+    @classmethod
+    def from_math_expression(cls, expression, node_dict, mod_val=19):
+        """
+        Create or update a node from a given mathematical expression.
+        
+        Parameters:
+        - expression: A string representing the mathematical expression of the node.
+        - node_dict: A dictionary of existing nodes to reference.
+        
+        Returns:
+        - The updated DAGWeightedNode instance.
+        """
+        parts = expression.split("=")
+        node_name = parts[0].strip()
+        rhs = parts[1].strip()
+        terms = rhs.split()
+        node = cls(node_name)
+        
+        i = 0
+        while i < len(terms):
+            if terms[i] in ['ADD', 'MUL', 'EQUL']:
+                operation = terms[i]
+                i += 1
+                parent_name = terms[i]
+                func = {'ADD': ADD, 'MUL': MUL, 'EQUL': EQUL}[operation](mod_val)
+                node.add_fan_in(node_dict[parent_name], func)
+            else:
+                parent_name = terms[i]
+                if parent_name.isdigit():
+                    node.weight = int(parent_name)
+                else:
+                    node.add_fan_in(node_dict[parent_name], EQUL(mod_val))
+            i += 1
+        node.compute_value()
+        return node
+    
 class AlgorithmicDAG:
     def __init__(self, 
                  vocab: List, 
@@ -336,7 +189,6 @@ class AlgorithmicDAG:
                  func_vocab: Optional[List] = [],
                  mod_val: Optional[int] = 19,
                  shuffle_predecessors: Optional[bool] = False,
-                 seed: Optional[int] = None, 
                  verbose: Optional[bool] = False,
                  vocab_obj: Optional[DAGWeightedNode] = None,
                     **kwargs
@@ -356,21 +208,21 @@ class AlgorithmicDAG:
         self.max_fan_in_deg = max_fan_in_deg
         self.max_depth = max_depth
 
-        if seed is not None:
-            random.seed(seed)
+        # if seed is not None:
+        #     random.seed(seed)
         
         self.func_vocab = func_vocab if func_vocab else [ADD, MUL]  # Default to addition and multiplication
 
-        self.node_info = {}
+        self.node_info = {node.name: node for node in (vocab_obj or [])}
         for node in vocab:
-            if vocab_obj and any(n.name == node for n in vocab_obj):
-                self.node_info[node] = next(n for n in vocab_obj if n.name == node)
-            else:
+            if node not in self.node_info:
                 self.node_info[node] = DAGWeightedNode(node)
                 
         if shuffle_predecessors:
             # shuffle the node_info dictionary to randomize the order of the nodes
-            self.node_info = dict(random.shuffle(list(self.node_info.items())))
+            shuffled_items = list(self.node_info.items())
+            random.shuffle(shuffled_items)
+            self.node_info = dict(shuffled_items)
         
         self.vocab = [node.name for node in self.node_info.values()] # already in order
 
@@ -504,30 +356,51 @@ class AlgorithmicDAG:
             node.weight = values[i] % self.mod_val
 
 
-def generate_two_level_dag(data_config: EasyDict):
+def generate_abs_dag(data_config: EasyDict):
 
     # Preparing the vocab for the DAG
-    num_abs_nodes = data_config.num_abs_nodes
-    vocab = [f'h_{i + 1}' for i in range(num_abs_nodes)]
+    num_nodes = data_config.num_nodes
+    vocab = [f'h_{i + 1}' for i in range(num_nodes)]
 
     if data_config.func_vocab:
         func_vocab = [globals()[func_name] for func_name in data_config.func_vocab]
     else:
         func_vocab = None
     # Initializing the first AlgorithmicDAG object
-    algorithmic_dag_1 = AlgorithmicDAG(
+    abs_dag = AlgorithmicDAG(
         vocab=vocab,
         min_fan_in_deg=data_config.min_fan_in_deg,
         max_fan_in_deg=data_config.max_fan_in_deg,
-        num_leaf_nodes=data_config.num_abs_leaf_nodes,
+        num_leaf_nodes=data_config.num_leaf_nodes,
         func_vocab=func_vocab,
         mod_val=data_config.mod_val,
         shuffle_predecessors=False,
-        seed=data_config.random_seed,
         verbose=data_config.verbose
     )
+    return abs_dag
 
-    # The first AlgorithmicDAG object has been initialized with vocab ['h_1', 'h_2', ...].
+def generate_ins_dag(abs_dag, data_config: EasyDict):
+    # Preparing the vocab for the ins_dag
+    num_nodes = data_config.num_nodes
+    vocab = [f'v_{i + 1}' for i in range(num_nodes)]
+
+    if data_config.func_vocab:
+        func_vocab = [globals()[func_name] for func_name in data_config.func_vocab]
+    else:
+        func_vocab = None
+    # Initializing the ins_dag with the leaf nodes of abs_dag as vocab_obj
+    ins_dag = AlgorithmicDAG(
+        vocab=vocab,
+        min_fan_in_deg=data_config.min_fan_in_deg,
+        max_fan_in_deg=data_config.max_fan_in_deg,
+        num_leaf_nodes=data_config.num_leaf_nodes,
+        func_vocab=func_vocab,
+        mod_val=data_config.mod_val,
+        shuffle_predecessors=True,
+        verbose=data_config.verbose,
+        vocab_obj=abs_dag.leaf_nodes_pointer
+    )
+    return ins_dag
 
 
 if __name__ == "__main__":
@@ -536,4 +409,24 @@ if __name__ == "__main__":
     config = ConfigBase(config_dir)
 
     data_config = config.data_config
-    generate_two_level_dag(data_config)
+    abs_dag = generate_abs_dag(data_config.abs_dag_config)
+    ins_dag = generate_ins_dag(abs_dag, data_config.ins_dag_config)
+
+    # set the leaf nodes of the ins_dag 
+    ins_dag.set_leaf_nodes_value(range(data_config.ins_dag_config.num_leaf_nodes))
+    # propogate the values to the rest of the nodes
+    ins_dag.sync_node_values()
+    abs_dag.sync_node_values()
+    # print the nodes
+
+    ins_dag.print_nodes()
+    abs_dag.print_nodes()
+
+    # randomly take a node from ins_dag
+    node = random.choice(list(ins_dag.graph.nodes))
+    math_expr = ins_dag.node_info[node].to_math_expression()
+    print(f"Node: {node}, Math Expression: {math_expr}")
+
+    # reconstruct the node from the math expression
+    node = DAGWeightedNode.from_math_expression(math_expr, ins_dag.node_info, mod_val=ins_dag.mod_val)
+    node.print_algorithmic_expression()
