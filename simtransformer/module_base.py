f4975f745e63df30758cfe8c49801a9383235400
Siyu <siyuc3141@gmail.com>
2024-10-17 02:16:49 -0400

    add num_workers

--- simtransformer/module_base.py
+++ simtransformer/module_base.py
@@ -570,22 +570,28 @@ class DataModuleBase(lightning.LightningDataModule):
             self.data_predict = clever_load(self.dir_handler.data_path)
 
     def train_dataloader(self):
+        num_workers = 4 * torch.cuda.device_count()
         return DataLoader(self.data_train, 
                           batch_size=self.data_config.batch_size, 
                           collate_fn=lambda x: x,
-                          shuffle=True)
+                          shuffle=True,
+                          num_workers=num_workers)
     
     def val_dataloader(self):
+        num_workers = 2 * torch.cuda.device_count()
         return DataLoader(self.data_val, 
                           batch_size=self.data_config.batch_size, 
                           collate_fn=lambda x: x,
-                          shuffle=False)
+                          shuffle=False,
+                          num_workers=num_workers)
     
     def test_dataloader(self):
+        num_workers = 2 * torch.cuda.device_count()
         return DataLoader(self.data_test, 
                           batch_size=self.data_config.batch_size, 
                           collate_fn=lambda x: x,
-                          shuffle=False)
+                          shuffle=False,
+                          num_workers=num_workers)
     
     def predict_dataloader(self):
         return DataLoader(
