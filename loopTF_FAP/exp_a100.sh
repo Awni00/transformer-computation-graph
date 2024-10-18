896573372957be7c8bb34ae59c3fc15523cb2ec2
Siyu <siyuc3141@gmail.com>
2024-10-18 02:01:32 -0400

    update

--- loopTF_FAP/exp_a100.sh
+++ loopTF_FAP/exp_a100.sh
@@ -29,12 +29,15 @@ conda activate scgpt1
 
 # Array of different configurations
 declare -a configs=(
-    "--use_ntp_loss False --max_dep 2 --add_med_loss_prob 0.1 1.0"
-    "--use_ntp_loss False --max_dep 4 --add_med_loss_prob 0.001 0.01 0.1 1.0"
-    "--use_ntp_loss False --max_dep 6 --add_med_loss_prob 0.00001 0.0001 0.001 0.01 0.1 1.0"
     "--use_ntp_loss False --max_dep 2 --add_med_loss_prob 1.0 1.0"
     "--use_ntp_loss False --max_dep 4 --add_med_loss_prob 1.0 1.0 1.0 1.0"
     "--use_ntp_loss False --max_dep 6 --add_med_loss_prob 1.0 1.0 1.0 1.0 1.0 1.0"
+    "--use_ntp_loss False --max_dep 6 --add_med_loss_prob 0.0 0.0 0.0 0.0 0.0 1.0"
+    "--use_ntp_loss False --max_dep 6 --add_med_loss_prob 0.0 0.0 1.0 0.0 0.0 1.0"
+    "--use_ntp_loss False --max_dep 6 --add_med_loss_prob 0.0 1.0 0.0 1.0 0.0 1.0"
+    "--use_ntp_loss False --max_dep 6 --add_med_loss_prob 0.00001 0.0001 0.001 0.01 0.1 1.0"
+    "--use_ntp_loss False --max_dep 6 --add_med_loss_prob 0.00243 0.0081 0.027 0.09 0.3 1.0"
+    "--use_ntp_loss False --max_dep 6 --add_med_loss_prob 0.03125 0.0625 0.125 0.25 0.5 1.0"
 )
 
 # Loop through configurations and submit each as a separate job
