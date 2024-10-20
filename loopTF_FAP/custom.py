335265a493fc9ce520a3ed5b62546d25494487fa
Siyu <siyuc3141@gmail.com>
2024-10-20 00:52:06 -0400

    update

--- loopTF_FAP/custom.py
+++ loopTF_FAP/custom.py
@@ -252,7 +252,7 @@ class Pipeline(PipelineBase):
                 # Here, we also add the token embedding to the hidden_state to make the model aware of the input, see https://arxiv.org/pdf/2409.15647 
             
             
-            # compute intermediate target loss at the '=' position
+            # compute intermediate target loss at the '=' position (for all previous and current depth to force the model to keep track of the variables)
             indices = torch.logical_and(torch.logical_and(dep <= cur_dep, msk_target == True), oper <= self.max_oper)
             med_target_loss, med_target_mrr, med_target_acc, num_target = self.intermediate_loss(hidden_state[:, 0:-1, :], y[:, 1:], indices[:, 1:], 'target', step_type, batch_size, cur_dep) 
             loss_ls.append(med_target_loss)
