fc5ac541c97897ce53eeeb7d23f91f197aa39a83
FFishy-git <siyuc3141@gmail.com>
2024-10-22 00:00:22 -0400

    add resumetrain_test file

--- resumetrain_test.py
+++ resumetrain_test.py
@@ -1,10 +1,12 @@
-from loopTF_FAP.experiments.starter import train_continue
+from loopTF_FAP.experiments.starter import train_continue, test_start
 
 # type = 'resumetrain'
 type = 'test'
+last_run_name = 'L2H16-N64-D6-medprob-[1.0e-1,1.0e+0,1.0e-1,1.0e+0,1.0e-1,1.0e+0]1018-1747'
+ckpt_file_name = 'epoch=173-val_loss=0.0505.ckpt'
 
 if __name__ == '__main__':
-    if type == 'resumetrain'
-        train_continue('L2H16-N64-D6-medprob-[1.0e-1,1.0e+0,1.0e-1,1.0e+0,1.0e-1,1.0e+0]1018-1747', 'epoch=173-val_loss=0.0505.ckpt')
+    if type == 'resumetrain':
+        train_continue(last_run_name, ckpt_file_name)
     elif type == 'test':
-        
+        test_start(last_run_name, ckpt_file_name)
